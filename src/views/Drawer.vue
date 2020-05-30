7b4d31735027ca4775349b4c3c1662a7eccab754
kasprzyk <piotkas1@gmail.com>
2020-05-30 15:15:48 +0200

    Drawer added

---
+++ src/views/Drawer.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <button @click="isOpen = !isOpen">
+      My Profile
+    </button>
+
+    <transition
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @leave="leave"
+      :css="false"
+    >
+      <div v-if="isOpen" class="drawer">
+        <img src="../assets/avatar.png" alt="avatar" />
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
+    </transition>
+  </div>
+</template>
