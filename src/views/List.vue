ca6944be0ba925978f1b132f861093d39575a2a3
kasprzyk <piotkas1@gmail.com>
2020-05-30 14:43:29 +0200

    another class added

--- src/views/List.vue
+++ src/views/List.vue
@@ -1,26 +1,32 @@
 <template>
   <div>
-    <input v-model="newContact" placeholder="Name" type="text" />
+    <input type="text" v-model="newContact" placeholder="Name" />
     <button @click="addContact">Add Contact</button>
-    <transition-group tag="ul">
+    <button @click="sortContacts">Sort</button>
+
+    <transition-group name="slide-up" tag="ul" appear>
       <li v-for="contact in contacts" :key="contact">
         {{ contact }}
       </li>
     </transition-group>
   </div>
 </template>
+
 <script>
 export default {
   data() {
     return {
       newContact: '',
-      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Wunderlind']
+      contacts: ['Beau Thabeast', 'Cindy Rella', 'Alice Vunderlind']
     }
   },
   methods: {
     addContact() {
       this.contacts.push(this.newContact)
       this.newContact = ''
+    },
+    sortContacts() {
+      this.contacts.sort()
     }
   }
 }
