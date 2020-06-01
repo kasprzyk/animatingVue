3640b7cf703660fea07d60c081647c5ef13fcbed
kasprzyk <piotkas1@gmail.com>
2020-06-01 13:41:08 +0200

    Router, App modified

--- src/router.js
+++ src/router.js
@@ -38,6 +38,15 @@ export default new Router({
       // which is lazy-loaded when the route is visited.
       component: () =>
         import(/* webpackChunkName: "list" */ './views/Cards.vue')
+    },
+    {
+      path: '/simple',
+      name: 'simple',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () =>
+        import(/* webpackChunkName: "list" */ './views/Simple.vue')
     }
   ]
 })
